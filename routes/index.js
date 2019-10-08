e5ea56273ba6dd1ed3b422c2b113cffcafb6df2b
RisingGeek <bhavaypuri15@gmail.com>
2019-10-08 22:45:06 +0530

    Add signup route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res, next) {
+  res.send({'key':'value'})
+});
+
 module.exports = router;
